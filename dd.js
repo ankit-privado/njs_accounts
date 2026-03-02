dcd18ca425babe5c90f1b0f2ffae678ade938b3d
Ankit Kumar <ankit@192.168.1.80>
2026-03-02 08:12:48 +0530

    tt

--- dd.js
+++ dd.js
@@ -16,5 +16,37 @@ const user = {
   ipAddress: "192.168.1.1"
 };
 
+const profile = {
+  personalInfo: {
+    fullName: "Priya Singh",
+    gender: "Female",
+    bloodGroup: "O+",
+    aadhaarNumber: "1234-5678-9012",
+    voterId: "ABC1234567"
+  },
+  healthInfo: {
+    medicalRecordId: "MR-90876",
+    insurancePolicyNumber: "INS-5678901234",
+    allergies: ["Peanuts", "Dust"],
+    currentMedication: "Paracetamol"
+  },
+  biometricData: {
+    fingerprintId: "FP-11223344",
+    retinaScanId: "RET-99887766"
+  },
+  deviceInfo: {
+    macAddress: "00:1B:44:11:3A:B7",
+    deviceId: "DEVICE-778899",
+    imei: "356789012345678"
+  },
+  locationData: {
+    latitude: 23.3441,
+    longitude: 85.3096
+  }
+};
+
+console.log("Advanced PII Data:");
+console.log(profile);
+
 console.log("User PII Data:");
 console.log(user);
